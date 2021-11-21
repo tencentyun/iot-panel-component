60684716e04d39cdb63dc6282b15fd163f7d26a1
shuaisguo <shuaisguo@tencent.com>
2021-11-21 10:36:51 +0800

    fix: 小数计算精度问题

--- src/NumberInput/index.md
+++ src/NumberInput/index.md
@@ -6,6 +6,11 @@
 import React from 'react';
 import { NumberInput } from 'qcloud-iot-panel-component';
 
-export default () => <NumberInput value={10} step={2} min={0} max={200} templateInfo={{define: {unit: '度'}}}/>
+export default () => (
+  <div>
+    <NumberInput value={10} step={2} min={0} max={200} templateInfo={{define: {unit: '度'}}}/>
+    <NumberInput value={10.1} step={0.2} min={0} max={200} templateInfo={{define: {unit: '度'}}}/>
+  </div>
+)
 ```
 <API></API>
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -7,3 +7,60 @@ export const isUrl = (url: string) => {
   return /(http:|https:)?\/\/.+/.test(url);
 };
 
+export const isFloatNumber = (num:number | string) =>{
+  return /\d.\d+/g.test(String(num));
+};
+
+export const getFloatDigit = function(number: number) {
+  const digit = String(number).split('.');
+  return (digit[1] == undefined) ? 0 : digit[1].length;
+};
+
+export enum arithmeticType {
+  add = 'add',
+  sub = 'sub',
+  multi = 'multi',
+  divide = 'divide',
+}
+export const arithmetic = (type: arithmeticType, firstNumber: number, secondNumber: number, fixLength = 2) => {
+  let result;
+      // 小数计算
+  if (isFloatNumber(firstNumber) || isFloatNumber(secondNumber)) {
+      const firstDecimalLength = getFloatDigit(firstNumber);
+      const secondDecimalLength = getFloatDigit(secondNumber);
+      const maxDecimalLength = firstDecimalLength >= secondDecimalLength ? firstDecimalLength : secondDecimalLength;
+      const baseMultiSize = Math.pow(10, maxDecimalLength);
+      const addNumber = (type === arithmeticType.sub) ? -secondNumber : secondNumber;
+
+      if (type === arithmeticType.add || type === arithmeticType.sub) {
+          result = (firstNumber * baseMultiSize + addNumber * baseMultiSize) / baseMultiSize;
+          return Number(result.toFixed(maxDecimalLength || fixLength));
+      }
+
+      const newFirstNumber = Number(String(firstNumber).replace('.', ''));
+      const newSecondNumber = Number(String(secondNumber).replace('.', ''));
+      if (type === arithmeticType.multi) {
+          result = (newFirstNumber * newSecondNumber) / Math.pow(10, firstDecimalLength + secondDecimalLength);
+      }
+      if (type === arithmeticType.divide) {
+          result = ((newFirstNumber / newSecondNumber) * Math.pow(10, secondDecimalLength - firstDecimalLength));
+          result = Number(result.toFixed(fixLength));
+      }
+      return result;
+  }
+  // 正常计算
+  if (type === arithmeticType.add) {
+    result = firstNumber + secondNumber;
+  }
+  if (type === arithmeticType.sub) {
+    result = firstNumber - secondNumber;
+  }
+  if (type === arithmeticType.multi) {
+    result = firstNumber * secondNumber;
+  }
+  if (type === arithmeticType.divide) {
+    result = Number((firstNumber / secondNumber).toFixed(fixLength));// 还是会出现无理数
+  }
+
+  return result;
+};
