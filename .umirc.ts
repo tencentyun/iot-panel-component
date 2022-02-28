37f372ace6279a11fc1a8309805e51564b9fa98d
shuaisguo <shuaisguo@tencent.com>
2022-02-28 18:37:59 +0800

    fix(disabled): fix 组件禁用态样式

--- .umirc.ts
+++ .umirc.ts
@@ -12,5 +12,8 @@ export default {
   base: '/iot-panel-component/',
   publicPath: '/iot-panel-component/',
   description: '腾讯连连标准组件库',
-  styles: ['.__dumi-default-device{width: 375px!important; transform: scale(0.75); background: rgb(245,245,245)}']
+  styles: ['.__dumi-default-device{width: 375px!important; transform: scale(0.75); background: rgb(245,245,245)}'],
+  scripts: [`if (location.href.match(/\\/~demos\\//)) {
+    document.body.style = 'background: rgb(245,245,245);max-width: 500px; margin: 0 auto;';
+  }`]
 }
